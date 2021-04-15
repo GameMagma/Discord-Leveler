3860fe31704170f9a745bd34308c18d0cd206de7
Connor Midgley <53975931+GameMagma@users.noreply.github.com>
2021-04-15 08:19:09 -0500

    Created longRun ability
    Added the ability to leave the bot on for 8 hours at a time, and also have it loop through the file instead of ending when it reached the end of the file. The length of this time can be adjusted by changing the "maxtime" variable in the typeWordsWithoutConfirmation function. Will possibly make this more user-friendly in the future.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileSystems;
-import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Main {
@@ -12,6 +11,58 @@ public class Main {
     public static void main(String[] args) {
         final String PATH = getCurrentDir() + File.separator + "src" + File.separator + "words.txt";
 
+        Scanner mainUserInput = new Scanner(System.in);
+
+        System.out.println("Short or long run? (Short/Long): ");
+        String choice = mainUserInput.nextLine().toLowerCase();
+
+        if (choice.equals("short")) {
+            shortRun(PATH);
+        } else if (choice.equals("long")) {
+            longRun(PATH);
+        } else {
+            System.out.println("Invalid input.");
+        }
+    }
+
+    /**
+     * Runs through the program just once, asking everytime it reaches the maximum words if you want to stop.
+     *
+     * @param PATH The path of the text file to get the words from
+     */
+    private static void shortRun(final String PATH) {
+        try {
+            Typer typer = new Typer();
+            File file = new File(PATH);
+            Scanner scanner = new Scanner(file);
+
+            System.out.println("File Exists: " + file.exists() + ". Path: " + file.getPath()); // Log whether the file exists/could be found and the path it was found at
+
+            System.out.println("Success. Typing now.");
+
+            String[] words = getWordsFromFile(scanner); // Get all words from the file, and put it into an array
+
+            typer.typeWords(words, SECONDS, MAXIMUM_WORDS); // Call method to type words repeatedly
+
+        } catch (AWTException e) {
+            System.err.println("Failed to create robot. Exiting program.\n");
+            e.printStackTrace();
+            System.exit(2);
+        } catch (IOException e) {
+            System.err.println("Could not find file. Exiting program.\n");
+            e.printStackTrace();
+            System.exit(3);
+        }
+    }
+
+    /**
+     * Runs the code repeatedly, but does not ask for confirmation every so often. Also loops around to the beginning
+     * of the file if it runs out of words. Will stop after a preset amount of time, but this time is generally around 8
+     * or so hours.
+     *
+     * @param PATH The path of the text file to get the words from
+     */
+    private static void longRun(final String PATH) {
         try {
             Typer typer = new Typer();
             File file = new File(PATH);
@@ -23,7 +74,7 @@ public class Main {
 
             String[] words = getWordsFromFile(scanner);
 
-            typer.typeWords(words, SECONDS, MAXIMUM_WORDS);
+            typer.typeWordsWithoutConfirmation(words, SECONDS);
 
         } catch (AWTException e) {
             System.err.println("Failed to create robot. Exiting program.\n");
--- src/Typer.java
+++ src/Typer.java
@@ -30,14 +30,6 @@ public class Typer {
             r.delay(delay);
 
             // Type word
-            /*for (int letter = 0; letter < currentWord.length(); letter++) {
-                char currentChar = currentWord.charAt(letter);
-
-
-                r.keyPress(currentChar);
-                r.keyRelease(currentChar);
-            } */
-
             writeString(currentWord);
 
             r.keyPress(KeyEvent.VK_ENTER); // Enter word
@@ -49,7 +41,6 @@ public class Typer {
                 System.out.println("Keep typing? y/n:");
 
                 String choice = userInput.next();
-                //userInput.reset(); // Clears the buffer
 
                 stay = (!choice.equals("n") && i < 500);
 
@@ -61,6 +52,46 @@ public class Typer {
 
                 wordCount = 0; // Resets the counter
             }
+        }
+    }
+
+    /**
+     * Types words repeatedly
+     *
+     * @param words An array of the available words
+     * @param seconds How many seconds in between each word
+     */
+    public void typeWordsWithoutConfirmation(String[] words, int seconds) {
+        int delay = seconds * 1000;
+        int i = 0; // Index for the word
+        boolean stay = true; // If program should keep typing
+
+        // Timer variables
+        long start = System.currentTimeMillis(); // The current time in milliseconds
+        //final long maxTime = 28800000; // 8 hours in milliseconds
+        final long maxTime = 3600000; // 1 hour in milliseconds
+
+        Scanner userInput = new Scanner(System.in); // Scanner for user input
+        r.delay(5000);
+        while (stay) {
+            String currentWord = words[i];
+
+            r.delay(delay);
+
+            // Type word
+            writeString(currentWord);
+
+            r.keyPress(KeyEvent.VK_ENTER); // Enter word
+
+            i++; // Increment index
+
+            long currentTime = System.currentTimeMillis() - start;
+
+            stay = (currentTime < maxTime);
+
+            System.out.println("Current time: " + currentTime + " | i = " + i + " | Stay = " + stay + " | Word Printed: " + currentWord); // Log info to console for debugging
+
+            if (i >= 500) { i = 0; }
         }
     }
 
